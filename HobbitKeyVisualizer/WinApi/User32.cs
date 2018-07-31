c6eaaca229600674ad0a3ad759ffea468984144f
Jacob Hagel <jacobhhagel@gmail.com>
2018-07-31 12:59:56 +0200

    Add User32-Function SetWindowsHookEx

---
+++ HobbitKeyVisualizer/WinApi/User32.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HobbitKeyVisualizer.WinApi
+{
+    internal class User32
+    {
+        private const string user32 = "user32.dll";
+
+        [DllImport(user32)]
+        public static extern int SetWindowsHookEx(
+            [In] int idHook,
+            [In] Delegate lpfn,
+            [In] IntPtr hMod,
+            [In] uint dwThreaedId = 0);
+    }
+}
